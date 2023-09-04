92817771cc657e11e89719c82fdfad671ce751e5
manuelscl <manuelscardenaslopez@gmail.com>
2023-09-04 15:24:01 -0600

    Separación de variables en un archivo independiente

--- js/script.js
+++ js/script.js
@@ -1,23 +1,4 @@
-const textarea = document.querySelector('.encrypt__text');
-const btnEncrypt = document.querySelector('.btn__encrypt');
-const btnDecrypt = document.querySelector('.btn__decrypt');
-const infoMessageContainer = document.querySelector('.message__info');
-const displayedMessageContainer = document.querySelector('#messageToShow');
-const btnCopy = document.querySelector('.btn__copy');
-
-const copyIcon = document.createElement('i');
-copyIcon.classList.add('bx', 'bx-copy-alt');
-const checkedIcon = document.createElement('i');
-checkedIcon.classList.add('bx', 'bx-check', 'inactive');
-checkedIcon.style.fontSize = '20px';
-
-const letterMappings = {
-    'a': 'ai',
-    'e': 'enter',
-    'i': 'imes',
-    'o': 'ober',
-    'u': 'ufat',
-};
+import {textarea, infoMessageContainer, displayedMessageContainer, btnEncrypt, btnDecrypt, btnCopy, copyIcon, checkedIcon, letterMappings} from './variables.js';
 
 function cleanMessage() {
     infoMessageContainer.classList.remove('inactive');
---
+++ js/variables.js
@@ -0,0 +1,23 @@
+const textarea = document.querySelector('.encrypt__text');
+const btnEncrypt = document.querySelector('.btn__encrypt');
+const btnDecrypt = document.querySelector('.btn__decrypt');
+const infoMessageContainer = document.querySelector('.message__info');
+const displayedMessageContainer = document.querySelector('#messageToShow');
+const btnCopy = document.querySelector('.btn__copy');
+
+const copyIcon = document.createElement('i');
+copyIcon.classList.add('bx', 'bx-copy-alt');
+const checkedIcon = document.createElement('i');
+checkedIcon.classList.add('bx', 'bx-check', 'inactive');
+checkedIcon.style.fontSize = '20px';
+
+const letterMappings = {
+    'a': 'ai',
+    'e': 'enter',
+    'i': 'imes',
+    'o': 'ober',
+    'u': 'ufat',
+};
+
+// export * from './variables.js';
+export {textarea, btnEncrypt, btnDecrypt, btnCopy, infoMessageContainer, displayedMessageContainer, letterMappings, copyIcon, checkedIcon};
