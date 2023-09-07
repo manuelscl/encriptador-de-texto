62bd7f511929b5ab199a0e021561892d51be2485
manuelscl <manuelscardenaslopez@gmail.com>
2023-09-07 11:19:29 -0600

    Refactorización en las funciones para cambiar a 'darkMode' o 'lightMode'

--- js/dark.js
+++ js/dark.js
@@ -2,17 +2,25 @@ const htmlElement = document.documentElement;
 const moonIcon = document.querySelector('.moon-icon');
 const sunIcon = document.querySelector('.sun-icon');
 
-moonIcon.addEventListener('click', () => {
+const switchToDarkMode = () => {
     sunIcon.classList.remove('inactive');
     moonIcon.classList.add('inactive');
     htmlElement.classList.add('dark');
+};
+
+const switchToLightMode = () => {
+    sunIcon.classList.add('inactive');
+    moonIcon.classList.remove('inactive');
+    htmlElement.classList.remove('dark');
+};
+
+moonIcon.addEventListener('click', () => {
+    switchToDarkMode();
     localStorage.setItem('mode', 'dark');
 });
 
 sunIcon.addEventListener('click', () => {
-    sunIcon.classList.add('inactive');
-    moonIcon.classList.remove('inactive');
-    htmlElement.classList.remove('dark');
+    switchToLightMode();
     localStorage.setItem('mode', 'light');
 });
 
@@ -21,13 +29,9 @@ window.addEventListener('load', () => {
     const modoGuardado = localStorage.getItem('mode');
 
     if(modoGuardado === 'dark') {
-        sunIcon.classList.remove('inactive');
-        moonIcon.classList.add('inactive');
-        htmlElement.classList.add('dark');
+        switchToDarkMode();
     } 
     else {
-        sunIcon.classList.add('inactive');
-        moonIcon.classList.remove('inactive');
-        htmlElement.classList.remove('dark');
+        switchToLightMode();
     }
 });
