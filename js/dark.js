a49e13448c70ee2eea6b8c49f4832dafddcb3396
manuelscl <manuelscardenaslopez@gmail.com>
2023-09-07 11:09:45 -0600

    Agregando el modo 'light' y 'dark' en localStorage

--- js/dark.js
+++ js/dark.js
@@ -6,10 +6,28 @@ moonIcon.addEventListener('click', () => {
     sunIcon.classList.remove('inactive');
     moonIcon.classList.add('inactive');
     htmlElement.classList.add('dark');
+    localStorage.setItem('mode', 'dark');
 });
 
 sunIcon.addEventListener('click', () => {
     sunIcon.classList.add('inactive');
     moonIcon.classList.remove('inactive');
     htmlElement.classList.remove('dark');
+    localStorage.setItem('mode', 'light');
+});
+
+
+window.addEventListener('load', () => {
+    const modoGuardado = localStorage.getItem('mode');
+
+    if(modoGuardado === 'dark') {
+        sunIcon.classList.remove('inactive');
+        moonIcon.classList.add('inactive');
+        htmlElement.classList.add('dark');
+    } 
+    else {
+        sunIcon.classList.add('inactive');
+        moonIcon.classList.remove('inactive');
+        htmlElement.classList.remove('dark');
+    }
 });
